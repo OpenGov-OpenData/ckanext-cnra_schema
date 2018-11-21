11b627a8c13341d5e588e94f6d25484c094eba03
postgres <gregor@greeninfo.org>
2018-11-21 10:35:54 -0800

    new template filter 'confirmjson' and use for spatial_details

--- ckanext/cnra_schema/plugins.py
+++ ckanext/cnra_schema/plugins.py
@@ -1,7 +1,11 @@
-from ckan.plugins import toolkit, IConfigurer, SingletonPlugin, implements
+from ckan.plugins import toolkit, IConfigurer, ITemplateHelpers, SingletonPlugin, implements
+import json
+from markupsafe import Markup
+
 
 class cnraSchema(SingletonPlugin):
     implements(IConfigurer)
+    implements(ITemplateHelpers)
 
     def update_config(self, config):
         toolkit.add_public_directory(config, "static")
@@ -14,3 +18,16 @@ ckanext.scheming:presets.json
         config['scheming.dataset_schemas'] = """
 ckanext.cnra_schema:schemas/dataset.yaml
 """
+
+    # define a dict of custom template functions for this schema
+    def get_helpers(self):
+        return {
+            'confirmjson': _json_decode_recode_string,
+        }
+
+# escape a possible JSON string, or else return ""
+def _json_decode_recode_string(string):
+    try:
+        return Markup(json.dumps(json.loads(string)))
+    except ValueError:
+        return json.dumps(None)
