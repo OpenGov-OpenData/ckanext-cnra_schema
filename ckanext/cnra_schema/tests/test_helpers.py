fe6cf5a8f3debef96d93488fe0518ded88f827e8
Javier Tello <jtello@opengov.com>
2020-09-03 00:54:52 -0400

    refactored unit tests

--- ckanext/cnra_schema/tests/test_helpers.py
+++ ckanext/cnra_schema/tests/test_helpers.py
@@ -4,144 +4,185 @@ import os
 import ckanext.cnra_schema.helpers as cnra_schema_helpers
 from nose.tools import assert_raises, assert_equal, assert_dict_equal, assert_list_equal, assert_false
 
-fixtures_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures')
-
-
-def get_iso_values():
-    with open(os.path.join(fixtures_path, 'iso_values.json'), 'r') as f:
-        result = json.load(f)
-    return result
-
-
-def get_modified_iso_values():
-    with open(os.path.join(fixtures_path, 'iso_values_without_harvest_job_config_values.json'), 'r') as f:
-        result = json.load(f)
-    return result
-
-
-def get_harvest_job_config():
-    with open(os.path.join(fixtures_path, 'harvest_job_config.json'), 'r') as f:
-        result = json.load(f)
-    return result
-
-
-def get_package_dict():
-    with open(os.path.join(fixtures_path, 'package_dict.json'), 'r') as f:
-        result = json.load(f)
-    return result
-
-
-def get_expected_modified_package_dict():
-    with open(os.path.join(fixtures_path, 'expected_extras_package_dict.json'), 'r') as f:
-        result = json.load(f)
-    return result
-
 
 class TestHelperFunctions(object):
 
-    assert_list_equal.__self__.maxDiff = None
-    assert_dict_equal.__self__.maxDiff = None
-
-    harvest_job_config = get_harvest_job_config()
-    iso_values = get_iso_values()
-    modified_iso_values = get_modified_iso_values()
-
     def test_delete_from_extras_success(self):
-        package_dict = get_package_dict()
+        package_dict = {'extras': [{'value':'x', 'key':'progress'}]}
         key = 'progress'
 
-        expected_modified_package_dict = get_expected_modified_package_dict()
-        del expected_modified_package_dict.get('extras')[16]
         cnra_schema_helpers.delete_existing_extra_from_package_dict(key, package_dict)
 
-        assert_list_equal(package_dict.get('extras'), expected_modified_package_dict.get('extras'))
+        assert_list_equal(package_dict.get('extras'), [])
 
+
+    def test_delete_from_extras_only_removes_key(self):
+        package_dict = {'extras': [{'value': 'x', 'key': 'progress'},{'value': 'y', 'key': 'new_key'}]}
+        key = 'progress'
+
+        cnra_schema_helpers.delete_existing_extra_from_package_dict(key, package_dict)
+
+        assert_list_equal(package_dict.get('extras'), [{'value': 'y', 'key': 'new_key'}])
 
     def test_delete_from_extras_failure(self):
-        package_dict = get_package_dict()
+        package_dict = {'extras': [{'value': 'x', 'key': 'progress'}]}
         key = 'backup_contact_email'
 
-        expected_modified_package_dict = get_expected_modified_package_dict()
         cnra_schema_helpers.delete_existing_extra_from_package_dict(key, package_dict)
 
-        assert_list_equal(package_dict.get('extras'), expected_modified_package_dict.get('extras'))
+        assert_list_equal(package_dict.get('extras'), [{'value': 'x', 'key': 'progress'}])
+
+    def test_delete_from_extras_missing_extras_fields(self):
+        package_dict = {}
+        key = 'backup_contact_email'
+
+        cnra_schema_helpers.delete_existing_extra_from_package_dict(key, package_dict)
+
+        assert_dict_equal(package_dict, {})
 
 
     def test_set_waf_map_fields_from_source_values(self):
-        package_dict = get_package_dict()
-        map_fields = self.harvest_job_config.get('map_fields', {})
-        modified_package_dict = cnra_schema_helpers.set_waf_map_fields(package_dict, self.iso_values,
+        package_dict = {}
+        map_fields = [{
+            'default': 'English',
+            'source':'language',
+            'target': 'language'
+        }]
+        iso_values = {'language': 'Spanish'}
+
+
+        modified_package_dict = cnra_schema_helpers.set_waf_map_fields(package_dict, iso_values,
                                                                              map_fields)
 
-        for map_field in map_fields:
-            target_field = map_field.get('target')
-            source_field = map_field.get('source')
-            iso_source_field = self.iso_values.get(source_field)
-
-            if isinstance(iso_source_field, list):
-                iso_source_field = ', '.join(str(x) for x in iso_source_field)
-
-            assert_equal(iso_source_field, modified_package_dict.get(target_field))
+        assert_equal('Spanish', modified_package_dict.get('language'))
 
 
     def test_set_waf_map_fields_from_default_values(self):
-        package_dict = get_package_dict()
-        map_fields = self.harvest_job_config.get('map_fields', {})
-        modified_package_dict = cnra_schema_helpers.set_waf_map_fields(package_dict, self.modified_iso_values,
+        package_dict = {}
+        map_fields = [{
+            'default': 'English',
+            'source': 'language',
+            'target': 'language'
+        }]
+        iso_values = {}
+
+        modified_package_dict = cnra_schema_helpers.set_waf_map_fields(package_dict, iso_values,
                                                                              map_fields)
 
-        for map_field in map_fields:
-            target_field = map_field.get('target')
-            default_field = map_field.get('default')
+        assert_equal('English', modified_package_dict.get('language'))
 
-            if isinstance(target_field, list):
-                target_field = ', '.join(str(x) for x in target_field)
+    def test_set_waf_map_fields_multi_value_map_field(self):
+        package_dict = {}
+        map_fields = [
+            {
+                'default': 'English',
+                'source': 'language',
+                'target': 'language',
+            },
+            {
+                'default': 'Canada',
+                'source': 'spatial',
+                'target': 'spatial_coverage'
+            }
+        ]
+        iso_values = {}
 
-            assert_equal(default_field, modified_package_dict.get(target_field))
+        modified_package_dict = cnra_schema_helpers.set_waf_map_fields(package_dict, iso_values,
+                                                                       map_fields)
+
+        assert_dict_equal({'language':'English', 'spatial_coverage':'Canada'}, modified_package_dict)
+
+
+    def test_set_waf_map_fields_value_is_list(self):
+        package_dict = {}
+        map_fields = [{
+            'default': '00',
+            'source': 'coordinate',
+            'target': 'coordinate'
+        }]
+        iso_values = {'coordinate':['-75.57563781738281','-69.82928466796875']}
+
+        modified_package_dict = cnra_schema_helpers.set_waf_map_fields(package_dict, iso_values,
+                                                                       map_fields)
+
+        assert_equal('-75.57563781738281, -69.82928466796875', modified_package_dict.get('coordinate'))
+
+
+    def test_set_waf_map_fields_empty_map_field(self):
+        package_dict = {}
+        map_fields = []
+        iso_values = {}
+        modified_package_dict = cnra_schema_helpers.set_waf_map_fields(package_dict, iso_values,
+                                                                       map_fields)
+
+        assert_dict_equal({}, package_dict)
 
 
     def test_set_waf_publisher_values_from_iso_values(self):
-        package_dict = get_package_dict()
-        publisher_mapping = self.harvest_job_config.get('publisher', {})
-        modified_package_dict = cnra_schema_helpers.set_waf_publisher_values(package_dict, self.iso_values, publisher_mapping)
+        package_dict = {}
+        publisher_mapping = {'publisher_field':'publisher', 'default_publisher':'Default-Publisher'}
+        iso_values = {'publisher':'Test-User'}
+        modified_package_dict = cnra_schema_helpers.set_waf_publisher_values(package_dict, iso_values, publisher_mapping)
 
-        assert_equal(self.iso_values.get('publisher'), modified_package_dict.get('publisher'))
+        assert_equal('Test-User', modified_package_dict.get('publisher'))
 
 
     def test_set_waf_publisher_values_from_harvest_job_config(self):
-        package_dict = get_package_dict()
-        publisher_mapping = self.harvest_job_config.get('publisher', {})
-        modified_package_dict = cnra_schema_helpers.set_waf_publisher_values(package_dict, self.modified_iso_values,
+        package_dict = {}
+        publisher_mapping = {'publisher_field':'publisher', 'default_publisher':'Default-Publisher'}
+        iso_values = {}
+        modified_package_dict = cnra_schema_helpers.set_waf_publisher_values(package_dict, iso_values,
                                                                              publisher_mapping)
 
-        assert_equal(publisher_mapping.get('default_publisher'), modified_package_dict.get('publisher'))
+        assert_equal('Default-Publisher', modified_package_dict.get('publisher'))
+
+
+    def test_set_waf_publisher_values_empty_publisher_mapping(self):
+        package_dict = {}
+        publisher_mapping = {}
+        iso_values = {}
+        modified_package_dict = cnra_schema_helpers.set_waf_publisher_values(package_dict, iso_values,
+                                                                             publisher_mapping)
+
+        assert_dict_equal({}, package_dict)
 
 
     def test_set_waf_contact_point_from_iso_values(self):
-        package_dict = get_package_dict()
-        contact_point_mapping = self.harvest_job_config.get('contact_point', {})
-        name_field = contact_point_mapping.get('name_field')
-        email_field = contact_point_mapping.get('email_field')
-        modified_package_dict = cnra_schema_helpers.set_waf_contact_point(package_dict, self.iso_values,
+        package_dict = {}
+        contact_point_mapping = {
+            'default_email':'test@default.com',
+            'name_field':'contact_name',
+            'default_name':'Default Name',
+            'email_field':'contact_email'
+        }
+        iso_values = {'contact':'John Smith', 'contact-email':'test@test.com'}
+        modified_package_dict = cnra_schema_helpers.set_waf_contact_point(package_dict, iso_values,
                                                                           contact_point_mapping)
 
-        iso_values_contact_name = self.iso_values.get('contact')
-        iso_values_contact_email = self.iso_values.get('contact-email')
 
-        assert_equal(iso_values_contact_name, modified_package_dict.get(name_field))
-        assert_equal(iso_values_contact_email, modified_package_dict.get(email_field))
+        assert_dict_equal({'contact_name':'John Smith', 'contact_email':'test@test.com'}, package_dict)
 
 
     def test_set_waf_contact_point_from_default_values(self):
-        package_dict = get_package_dict()
-        contact_point_mapping = self.harvest_job_config.get('contact_point', {})
-        name_field = contact_point_mapping.get('name_field')
-        email_field = contact_point_mapping.get('email_field')
-        modified_package_dict = cnra_schema_helpers.set_waf_contact_point(package_dict, self.modified_iso_values,
+        package_dict = {}
+        contact_point_mapping = {
+            'default_email': 'test@default.com',
+            'name_field': 'contact_name',
+            'default_name': 'Default Name',
+            'email_field': 'contact_email'
+        }
+        iso_values = {}
+        modified_package_dict = cnra_schema_helpers.set_waf_contact_point(package_dict, iso_values,
                                                                           contact_point_mapping)
 
-        default_values_contact_name = contact_point_mapping.get('default_name')
-        default_values_contact_email = contact_point_mapping.get('default_email')
+        assert_equal({'contact_name':'Default Name', 'contact_email':'test@default.com'}, package_dict)
 
-        assert_equal(default_values_contact_name, modified_package_dict.get(name_field))
-        assert_equal(default_values_contact_email, modified_package_dict.get(email_field))
+
+    def test_set_waf_contact_point_empty_mapping(self):
+        package_dict = {}
+        contact_point_mapping = {}
+        iso_values = {}
+        modified_package_dict = cnra_schema_helpers.set_waf_contact_point(package_dict, iso_values,
+                                                                          contact_point_mapping)
+
+        assert_dict_equal({}, package_dict)
